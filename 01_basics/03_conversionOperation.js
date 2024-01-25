19ef768be08b6e2bfa1b9ccef8fcb9f3f6fbc4fd
Kunal-Bachhav <148333128+Kunal-Bachhav@users.noreply.github.com>
2024-01-25 15:52:37 +0000

    type conversion done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33";
+console.log(typeof score);
+
+let conversion = Number(score);
+console.log(typeof conversion)
+
+/*
+
+"33" => 33
+"33abc" => NaN(Not a Number) 
+true => 1
+false => 0
+
+*/
+
+let isLoggin = 1;
+let booleanIsLoggin = Boolean(isLoggin)
+console.log(booleanIsLoggin)
+
+/*
+
+1 => true
+" " => false
+"kunal" => true
+
+*/
