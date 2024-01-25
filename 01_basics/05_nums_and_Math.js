e530f40fb9add52863f83a83fe2c81a4f111fa6d
Kunal-Bachhav <148333128+Kunal-Bachhav@users.noreply.github.com>
2024-01-25 17:18:19 +0000

    mathNums

---
+++ 01_basics/05_nums_and_Math.js
@@ -0,0 +1,14 @@
+const score = 200
+console.log(score);
+
+const balance = new Number(score)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+//=========================Maths========================
+
+console.log(Math);
+console.log(Math.abs(-7));
+console.log(Math.round(14.29));
