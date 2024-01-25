05792e32f9e93422af978cb13320300e32f610c3
Kunal-Bachhav <148333128+Kunal-Bachhav@users.noreply.github.com>
2024-01-25 17:18:05 +0000

    mathNums

---
+++ 01_basics/04_Strings.js
@@ -0,0 +1,12 @@
+const name = "kunal"
+const repoCount = 50;
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+// ye modern way hain syntx ka ,isko interpoletion bhi bola jata hain
+
+
+const gameName = new String('KunalBachhav')
+console.log(gameName.length);
+console.log(gameName.toUpperCase())
+console.log(gameName.toLowerCase())
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('B'));
---
+++ 01_basics/dataTypes_summery.js
@@ -0,0 +1,24 @@
+// Primitive
+// 7 types = String , Number , null , Boolean , undefine , Symbol , BigInt
+
+// Reference-Type(non-primitive)
+// Arrays , Object , Functions
+
+
+const myfunction = function(){
+    console.log("hello world");
+}
+
+console.log(myfunction)
+
+// Stack memory(Primitive type of data)
+/*
+  jab hum primitive type ke value main change krenge, toh uski copy hui value main change hogga
+   matlab original value main change nhi hoga
+*/
+
+
+// Heap memory(Non Primitive type of data)
+/*
+  jab hum Non-primitive type ke value main change krenge, toh uski Original value main change hogga
+*/
